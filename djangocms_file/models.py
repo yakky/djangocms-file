da6f0d2ca796d1bdc634690a793b06e69b472015
Iacopo Spalletti <i.spalletti@nephila.it>
2016-03-05 03:04:22 +0100

    Use python_2_unicode_compatible from Django, not the CMS. Use get_plugin_media_path from the CMS

--- djangocms_file/models.py
+++ djangocms_file/models.py
@@ -1,21 +1,11 @@
 import os
 
+from cms.models import get_plugin_media_path, CMSPlugin
 from django.db import models
 from django.conf import settings
 from django.core.files.storage import default_storage
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
-
-from cms.models import CMSPlugin
-try:
-    from cms.models import get_plugin_media_path
-except ImportError:
-    def get_plugin_media_path(instance, filename):
-        """
-        See cms.models.pluginmodel.get_plugin_media_path on django CMS 3.0.4+
-        for information
-        """
-        return instance.get_media_path(filename)
-from cms.utils.compat.dj import python_2_unicode_compatible
 
 
 @python_2_unicode_compatible
